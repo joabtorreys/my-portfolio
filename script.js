88afc6c2c3c1949d03be5859f7d771f96e6dda3a
iamjoey <j.torreys@outlook.com>
2018-04-26 13:59:53 -0400

    start point

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+  // start
+
+  $('.header_link').mouseenter(function() {
+    $(this).css('color', 'red')
+  });
+
+
+  $('.header_link').mouseleave(function() {
+    $(this).css('color', '#FFEB3B')
+  });
+  // section
+  $('.img_link').mouseenter(function() {
+    $(this).css('color', 'red')
+  });
+
+
+  $('.img_link').mouseleave(function() {
+    $(this).css('color', '#c1b54a')
+  });
+  // section
+  $('.proj_link').mouseenter(function() {
+    $(this).css('color', 'red')
+  });
+
+
+  $('.proj_link').mouseleave(function() {
+    $(this).css('color', 'yellow')
+  });
+
+
+
+
+
+
+
+  // finish
+})
